7c1044685a342cc2c452d4eb7b05b2184fec5873
Armada6199 <moh.abdin27@gmail.com>
2023-07-15 21:25:54 +0300

    finishing lab 11

--- vendor/index.js
+++ vendor/index.js
@@ -1,2 +1,5 @@
-const Vendor=require('./handler');
-module.exports={Vendor};
+const ordersEvents=require('../eventPool');
+const orderInfo=require('./handler');
+setTimeout(()=>{
+    ordersEvents.emit('newOrder',orderInfo);
+},0);
