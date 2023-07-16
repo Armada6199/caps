bd1b453be03ab66ac2b4dba14c237a3f26b00255
Armada6199 <moh.abdin27@gmail.com>
2023-07-16 18:13:59 +0300

    basic strucutre

--- clients/flower-vendor/index.js
+++
@@ -1,6 +0,0 @@
-const ordersEvents=require('../eventPool');
-const handlers=require('./handler');
-const {handlePickup,handleInTransite,handleDropOff}=handlers;
-ordersEvents.on('driverPickup',handlePickup);
-ordersEvents.on('inTransite',handleInTransite);
-ordersEvents.on('dropOff',handleDropOff);
